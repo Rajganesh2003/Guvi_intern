7cfa481f58c18fc17e2d6dae5adbbd9862acefdc
RAJGANESH A <98245825+Rajganesh2003@users.noreply.github.com>
2023-03-27 23:36:17 -0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,66 @@
+<?php
+
+require 'useractions.php';
+session_start();
+
+
+if(isset($_SESSION['loggedin'])){
+    $user = fetch_user_by_email($_SESSION['email']);
+
+?>    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+        <style>
+            .cen{
+                text-align: center;
+                margin-bottom: 2rem;
+            }
+            body{
+                text-align: center;
+                background-image: url("./image.jpg");
+                height: 100%;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            .cent{
+                text-decoration: none;
+                text-align: center;
+                margin-top: 2rem;                
+            }
+            .rem{
+                text-decoration: none;
+                padding: 1rem;
+                background-color: black;
+                color: bisque;
+            }
+            .rem:hover{
+                background-color: bisque;
+                color: black;
+            }
+
+
+        </style>
+    </head>
+    <body>
+        <h1 class="cen">Welcome <?=($user['gender']==='Male')?"Mr. ":"Ms. "?><?=$user['first_name']." ".$user['last_name']?></h1>
+        <h3 class="cen">Email : <?=$user['email']?></h3>
+        <h3 class="cen">Age : <?=$user['age']?></h3>
+        <h3 class="cen">Gender : <?=$user['gender']?></h3>
+        <h3 class="cen">Phone : <?=$user['mobile']?></h3>
+        <h2 class="cent"><a href="/guvi/logout.php" class="rem">Logout</a></h2>
+    </body>
+    </html>
+<?php
+}
+
+?>
+
+
+
+
